9e3ce2d98670813aaeebc26e6309e199956a820b
Sergio Revilla <sergio@unmundotecnologico.org>
2013-05-12 20:24:57 +0200

    created rails app

---
+++ juancar/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Juancar::Application.config.secret_token = '89f8a83876d9382583ebdd6a7c2f7efdb9127181a82ea86a2fc0b091cb6293969566c80cd041bfb4ff18ec60ad0ae508d9de8161cd9106b0f0794a47c6278ce1'
